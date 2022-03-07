22860825f6eeca41fe3ea2ccb93ac84ed3dd08ba
abdul waheed <a.waheed3738@gmail.com>
2022-03-07 13:44:38 +0500

    initital commit for clients website

--- components/hero/Hero.jsx
+++ components/hero/Hero.jsx
@@ -6,7 +6,7 @@ function Hero() {
             <div>
                 <div className="container mx-auto flex flex-col items-center py-12 sm:py-24">
                     <div className="w-11/12 sm:w-2/3 mb-5 sm:mb-10">
-                        <h1 className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl xl:text-6xl text-center text-gray-800 font-extrabold leading-tight">
+                        <h1 className="text-4xl sm:text-5xl md:text-5xl lg:text-5xl xl:text-6xl text-center text-gray-800 font-extrabold leading-tight">
                             Gain Your top Place in Digital Marketing with <span className="text-indigo-700">Sandiip Porwal</span>
                         </h1>
                         <p className="mt-5 sm:mt-10 text-gray-600 font-normal text-center text-lg sm:text-lg">Flexible Social Media Marketing Solutions to keep you ahead of competition. <br/> Digital Transformation Strategy that takes your business to the next level.</p>
