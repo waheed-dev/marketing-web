8d9643faf80750d2e23f5a8a00e78b6b23861228
waheed-dev <79965735+waheed-dev@users.noreply.github.com>
2022-03-09 21:36:22 +0500

    Add files via upload
    cahnged contact page layout

--- pages/contact.jsx
+++ pages/contact.jsx
@@ -7,42 +7,34 @@ const Contact = () => {
   return (
       <div className="py-12 2xl:px-20 px-6 xl:mx-auto xl:container">
           <div className="flex flex-col items-center">
-              <h1 className="text-5xl font-bold text-gray-800 text-center">Contact Us</h1>
-              <p className="text-base leading-normal text-center text-gray-600 mt-5 md:w-3/5">Want to Grow your Business with better Digital Marketing Results. We will Help You Get There.</p>
-              <p className="text-base leading-normal text-center text-gray-600 mt-5 md:w-3/5">Whatever your requirements or budget, our the talented team of digital experts at Sandiip Porwal Digital Marketing Consultancy is here and ready to help you.</p>
-              <p className="text-base leading-normal text-center text-gray-600 mt-5 md:w-3/5">We offer a first free consultation followed by a brainstorming session pre signup for a fee and then once you sign up for our digital marketing services you have with you a performance-driven digital agency as marketing partner.</p>
-
+              <h1 className="text-3xl font-bold text-gray-800 text-center w-11/12 sm:w-2/3 mb-5 sm:mb-10">Want to Grow your Business with better Digital Marketing Results. We will Help You Get There.</h1>
               <div
-                  className="container mx-auto grid sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 mt-10 gap-6">
-                  <div className="bg-white border rounded-md border-gray-200 h-64 flex flex-col items-center">
-                      <div className="w-16 mt-8 h-16 bg-orange-50 rounded-full flex items-center justify-center"
-                           role="img" aria-label="phone number">
-                          <BsTelephone size={50} color={'#ea580c'} />
-                      </div>
-                      <p className="text-xl mt-10 font-semibold leading-5 text-center text-gray-800">Phone</p>
-                      <p className="text-base mt-4 leading-4 text-center text-gray-600">(007) 095-6890</p>
+                  className="container mx-auto grid sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 mt-12 gap-6">
+                  <div className="bg-white border rounded-md border-gray-200 h-80 flex flex-col items-center px-4">
+                      <div className={'bg-green-400 h-8 w-2 absolute mr-96 ml-7  md:mr-96 md:ml-11 lg:mr-80 lg:ml-10'}></div>
+                      <p className="text-xl mt-10 font-semibold leading-5 text-center text-gray-800">RE-ENERGISE</p>
+                      <p className="text-gray-600 font-normal leading-7 text-left text-lg sm:text-lg mt-4 lg:w-8/10 sm:w-10/12">Whether it is slow revenue or you are unsure where to start with digital marketing or how to re-energise your digital marketing ?</p>
                   </div>
-                  <div className="bg-white border rounded-md border-gray-200 h-64 flex flex-col items-center">
-                      <div className="w-16 mt-8 h-16 bg-orange-50 rounded-full flex items-center justify-center"
-                           role="img" aria-label="email">
-                          <AiOutlineMail size={50} color={'#ea580c'} />
-                      </div>
-                      <p className="text-xl mt-10 font-semibold leading-5 text-center text-gray-800">Email</p>
-                      <p className="text-base mt-4 leading-4 text-center text-gray-600">Alphasquad@gmail.com</p>
+                  <div className="bg-white border rounded-md border-gray-200 h-80 flex flex-col items-center px-4">
+                      <div className={'bg-green-400 h-8 w-2 absolute mr-96 ml-7  md:mr-96 md:ml-11 lg:mr-80 lg:ml-10'}></div>
+                      <p className="text-xl mt-10 font-semibold leading-5 text-center text-gray-800">INVEST SMART</p>
+                      <p className="text-gray-600 font-normal leading-7 text-left text-lg sm:text-lg mt-4 lg:w-8/10 sm:w-10/12">Whatever your requirements or budget, our the talented team of digital experts at Sandiip Porwal Digital Marketing Consultancy is here and ready to help you.</p>
                   </div>
-                  <div className="bg-white border rounded-md border-gray-200 h-64 flex flex-col items-center">
-                      <div className="w-16 mt-8 h-16 bg-orange-50 rounded-full flex items-center justify-center"
-                           role="img" aria-label="location">
-                          <GoLocation size={50} color={'#ea580c'} />
-                      </div>
-                      <p className="text-xl mt-10 font-semibold leading-5 text-center text-gray-800">Address</p>
-                      <p className="text-base mt-4 leading-6 text-center text-gray-600 w-48">13420 -Oak Street, New York
-                          City (9134200)</p>
+                  <div className="bg-white border rounded-md border-gray-200 h-80 flex flex-col items-center px-4">
+                      <div className={'bg-green-400 h-8 w-2 absolute mr-96 ml-7  md:mr-96 md:ml-11 lg:mr-80 lg:ml-10'}></div>
+                      <p className="text-xl mt-10 font-semibold leading-5 text-center text-gray-800">FREE CONSULT</p>
+                      <p className="text-gray-600 font-normal leading-7 text-left text-lg sm:text-lg mt-4 lg:w-8/10 sm:w-10/12">We offer a first free consultation followed by a brainstorming session pre signup for a fee.</p>
+                  </div>
+
+                  <div className="bg-white border rounded-md border-gray-200 h-80 flex flex-col items-center px-4">
+                      <div className={'bg-green-400 h-8 w-2 absolute mr-96 ml-7  md:mr-96 md:ml-11 lg:mr-80 lg:ml-10'}></div>
+                      <p className="text-xl mt-10 font-semibold leading-5 text-center text-gray-800">SIGN UP</p>
+                      <p className="text-gray-600 font-normal leading-7 text-left text-lg sm:text-lg mt-4 lg:w-8/10 sm:w-10/12">Once you sign up for our digital marketing services you have with you a performance-driven digital agency as marketing partner.</p>
+              <button className=" hover:opacity-90 text-base font-normal bg-custom-1 rounded f-m-m md:py-2 py-2 px-2 mt-6 md:px-6 foucus:outline-none text-white">Drop us a line</button>
                   </div>
               </div>
           </div>
           <div className={'container mx-auto flex flex-col items-center py-10 sm:py-16'}>
-              <button className=" hover:opacity-90 text-base font-bold bg-custom-1 rounded f-m-m md:py-4 py-2 px-4 md:px-8 foucus:outline-none text-white">Drop us a line</button>
           </div>
       </div>
   )
