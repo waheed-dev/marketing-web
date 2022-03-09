d36e388c53697f31b222b87b25b43b7dd05d6526
waheed-dev <79965735+waheed-dev@users.noreply.github.com>
2022-03-10 00:48:49 +0500

    Add files via upload

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,10 +3,13 @@ import type { AppProps } from 'next/app'
 import Hero from "../components/hero/Hero";
 import Footer from "../components/footer/footer";
 import Navbar from "../components/navbar/Navbar";
-
+import Head from 'next/head'
 function MyApp({ Component, pageProps }: AppProps) {
   return (
       <div>
+          <Head>
+              <link rel="shortcut icon" href="/favicon.png" />
+          </Head>
           <Navbar/>
         <Component {...pageProps} />
         <Footer/>
